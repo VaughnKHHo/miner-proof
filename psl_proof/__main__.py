87d153f345fde89eac5dedb109c453820f905899
Vaughn <39765823+VaughnKHHo@users.noreply.github.com>
2025-07-07 15:43:17 +0200

    url

--- psl_proof/__main__.py
+++ psl_proof/__main__.py
@@ -19,7 +19,7 @@ def load_config() -> Dict[str, Any]:
         'input_dir': INPUT_DIR,
         'salt': '5EkntCWI',
         # 'validator_base_api_url': 'https://api.vana.genesis.dfusion.ai'
-        'validator_base_api_url': 'https://5c19-41-193-87-170.ngrok-free.app'
+        'validator_base_api_url': 'https://98e0-41-193-87-106.ngrok-free.app'
     }
     logging.info(f"Using config: {json.dumps(config, indent=2)}")
     return config
