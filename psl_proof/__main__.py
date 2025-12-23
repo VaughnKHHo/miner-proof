bbf79df4302098901fe80f79f724dc0785d789f6
Vaughn <vaughn@mysoft.co.za>
2025-12-23 18:39:56 +0200

    latest scoring

--- psl_proof/__main__.py
+++ psl_proof/__main__.py
@@ -15,11 +15,11 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 def load_config() -> Dict[str, Any]:
     """Load proof configuration from environment variables."""
     config = {
-        'dlp_id': 21,
+        'dlp_id': 4,
         'input_dir': INPUT_DIR,
         'salt': '5EkntCWI',
-        # 'validator_base_api_url': 'https://api.vana.genesis.dfusion.ai'
-        'validator_base_api_url': 'https://b1858a42a344.ngrok-free.app'
+        'validator_base_api_url': 'https://api.vana.genesis.dfusion.ai'
+        #'validator_base_api_url': 'https://9634-169-0-170-71.ngrok-free.app'
     }
     logging.info(f"Using config: {json.dumps(config, indent=2)}")
     return config
